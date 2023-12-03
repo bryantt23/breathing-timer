f5d67b89d803c627efd43459211ff4d61f366cae
bryant tunbutr <bryantt23@users.noreply.github.com>
2023-12-02 23:40:44 -0800

    create plan

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 alert('hiii');
+/*
+TODO
+
+states:
+not running
+running
+    inhale
+    hold
+    exhale
+finished
+    messages: congrats you rock
+    color: light blue
+
+messages:     
+    inhale: Breathe in through the nose for {x} seconds, feel the stomach pushing out
+    hold: Hold for {x*4} seconds, feel the oxygen coursing through your body
+    exhale: Breathe out through the mouth for {x*2} seconds, feel the stomach pulling in
+
+colors: 
+    inhale: green
+    hold: yellow
+    exhale: red
+
+breathCount:
+secondsRemaining
+
+ui
+not running
+    have seconds for input & start button
+running
+    reset button
+
+sequence
+enter seconds (default 4)
+hit start button
+
+loop 10 times
+    inhale
+    hold
+    exhale
+
+show finish message
+reset    
+
+maybe for each say what to do & have a countdown 
+
+*/
