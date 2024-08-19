2baee7a004558464b1741ca0f59dbc72fcb44d55
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-08-19 17:41:56 +0800

    base to be able to run locally

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/breathing-timer/',
+  base: process.env.NODE_ENV === 'production' ? '/breathing-timer/' : '/',
   plugins: [react()],
 })
